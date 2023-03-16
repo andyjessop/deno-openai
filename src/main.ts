4fcdb21ea788bea9d4f0345260450af3f5635e88
Andy Jessop <andrewdjessop@gmail.com>
2023-03-16 21:45:52 +0100

    Tabs...nein.

--- src/main.ts
+++ src/main.ts
@@ -3,29 +3,29 @@ import { dotenv, flags, openai } from './deps.ts';
 main();
 
 async function main() {
-	const {
-		model = 'text-davinci-003',
-		prompt = 'Write a haiku about the weather.',
-		temperature = 0,
-		maxTokens = 50,
-	} = flags.parse(Deno.args);
-	const config = await dotenv.load();
-	const ai = new openai.OpenAI(config.OPENAI_API_KEY);
+  const {
+    model = 'text-davinci-003',
+    prompt = 'Write a haiku about the weather.',
+    temperature = 0,
+    maxTokens = 50,
+  } = flags.parse(Deno.args);
+  const config = await dotenv.load();
+  const ai = new openai.OpenAI(config.OPENAI_API_KEY);
 
-	console.info('Creating a completion with parameters:');
-	console.info('----------------------------------');
-	console.info(`model: ${model}`);
-	console.info(`prompt: ${prompt}`);
-	console.info(`temperature: ${temperature}`);
-	console.info(`maxTokens: ${maxTokens}`);
-	console.info('----------------------------------\n');
+  console.info('Creating a completion with parameters:');
+  console.info('----------------------------------');
+  console.info(`model: ${model}`);
+  console.info(`prompt: ${prompt}`);
+  console.info(`temperature: ${temperature}`);
+  console.info(`maxTokens: ${maxTokens}`);
+  console.info('----------------------------------\n');
 
-	const completion = await ai.createCompletion({
-		model,
-		prompt,
-		temperature,
-		maxTokens,
-	});
+  const completion = await ai.createCompletion({
+    model,
+    prompt,
+    temperature,
+    maxTokens,
+  });
 
-	console.log(completion);
+  console.log(completion);
 }
